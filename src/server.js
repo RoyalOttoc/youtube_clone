ab4f609c5df1fbe1cfc0719e7b98a6e7d67cfb04
RoyalOttoc <jong.kim8706@gmail.com>
2021-04-30 16:44:37 +1000

    add router home,login

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ import express from "express";
 const app = express();
 const PORT = 4000;
 
-const handleListening = () => {
-    console.log(`✅ Server listening to http://localhost:${PORT}`)
-}
+const handleHome = (req, res) => res.send("Home")
+const handleLogin = (req, res) => res.send("Login")
 
+app.get('/', handleHome)
+app.get('/login', handleLogin)
+
+const handleListening = () => {
+    console.log(`✅ Server listening to http://localhost:${PORT} 🚀`)
+}
 app.listen(PORT, handleListening)
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
