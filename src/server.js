16d0fb551668c0bf057a95816187f1f75f0c8681
RoyalOttoc <jong.kim8706@gmail.com>
2021-05-02 22:44:58 +1000

    add morgan

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 import express from "express";
+import morgan from "morgan"
 
 const app = express();
+const logger = morgan("dev")
+
 const PORT = 4000;
 
 const handleHome = (req, res) => res.send("Home")
 const handleLogin = (req, res) => res.send("Login")
 
+app.use(logger)
 app.get('/', handleHome)
 app.get('/login', handleLogin)
 
