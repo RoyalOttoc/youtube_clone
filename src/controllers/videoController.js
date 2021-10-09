a2e41c31f0c819d93e3a94615479218f1dc8670b
Jong Kim <jong.kim8706@gmail.com>
2021-10-09 18:03:54 +1100

    add upload function

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -50,6 +50,24 @@ export const postEdit = (req, res) => {
   res.redirect(`/videos/${id}`);
 };
 
+export const getUpload = (req, res) => {
+  res.render('upload', { pageTitle: 'Upload video' });
+};
+
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: 'just now',
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  res.redirect('/');
+};
+
 export const search = (req, res) => res.send('search videos');
 export const upload = (req, res) => res.send('upload videos');
 export const deleteVideo = (req, res) => res.send('Delete videos');
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { watch, deleteVideo, getEdit, postEdit, upload } from '../controllers/videoController';
+import { watch, deleteVideo, getEdit, postEdit, upload, getUpload, postUpload } from '../controllers/videoController';
 
 const videoRouter = express.Router();
 videoRouter.get('/:id(\\d+)', watch);
 videoRouter.route('/:id(\\d+)/edit').get(getEdit).post(postEdit);
+videoRouter.route('/upload', upload).get(getUpload).post(postUpload);
 
-videoRouter.get('/upload', upload);
 videoRouter.get('/:id(\\d+)/delete', deleteVideo);
 
 export default videoRouter;
