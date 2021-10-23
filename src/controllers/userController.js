8f9d58454f0f43b3908917e3e6cfa28aecd34b84
Jong Kim <jong.kim8706@gmail.com>
2021-10-23 21:51:18 +1100

    update DB save with avatarUrl

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -101,13 +101,10 @@ export const finishGithubLogin = async (req, res) => {
     if (!emailObj) {
       res.redirect('/login');
     }
-    const existingUser = await User.findOne({ email: emailObj.email });
-    if (existingUser) {
-      req.session.loggedIn = true;
-      req.session.user = existingUser;
-      return res.redirect('/');
-    } else {
-      const user = await User.create({
+    let user = await User.findOne({ email: emailObj.email });
+    if (!user) {
+      user = await User.create({
+        avatarUrl: userData.avatar_url,
         name: userData.name,
         username: userData.login,
         email: emailObj.email,
@@ -115,10 +112,11 @@ export const finishGithubLogin = async (req, res) => {
         socialOnly: true,
         location: userData.location,
       });
-      req.session.loggedIn = true;
-      req.session.user = user;
-      return res.redirect('/');
     }
+
+    req.session.loggedIn = true;
+    req.session.user = user;
+    return res.redirect('/');
   } else {
     res.redirect('/login');
   }
