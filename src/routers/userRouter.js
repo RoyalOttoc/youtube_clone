fa2987660211a88b4dd2890cf00a084deb506b9e
Jong Kim <jong.kim8706@gmail.com>
2021-10-23 18:05:43 +1100

    add github login

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,7 +1,10 @@
 import express from 'express';
-import { handleEdit, handledelete, see } from '../controllers/userController';
+import { handleEdit, handledelete, see, startGithubLogin, finishGithubLogin } from '../controllers/userController';
 
 const userRouter = express.Router();
+userRouter.get('/github/start', startGithubLogin);
+userRouter.get('/github/finish', finishGithubLogin);
+
 userRouter.get('/edit', handleEdit);
 userRouter.get('/delete', handledelete);
 userRouter.get('/:id(\\d+)', see);
