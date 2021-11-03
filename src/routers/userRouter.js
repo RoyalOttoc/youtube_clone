a6c4fddadbb0f5c8871f8d5c05b1a19c705b9692
Jong Kim <jong.kim8706@gmail.com>
2021-11-04 02:45:48 +1100

    add video upload

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import {
-  handledelete,
   see,
   startGithubLogin,
   finishGithubLogin,
@@ -10,16 +9,15 @@ import {
   getChangePassword,
   postChangePassword,
 } from '../controllers/userController';
-import { protectorMiddleware, publicOnlyMiddleware, uploadFiles } from '../middlewares';
+import { avatarUpload, protectorMiddleware, publicOnlyMiddleware } from '../middlewares';
 
 const userRouter = express.Router();
 userRouter.get('/github/start', publicOnlyMiddleware, startGithubLogin);
 userRouter.get('/github/finish', publicOnlyMiddleware, finishGithubLogin);
 userRouter.get('/logout', protectorMiddleware, logout);
-userRouter.route('/edit').all(protectorMiddleware).get(getEdit).post(uploadFiles.single('avatar'), postEdit);
+userRouter.route('/edit').all(protectorMiddleware).get(getEdit).post(avatarUpload.single('avatar'), postEdit);
 userRouter.route('/change-password').all(protectorMiddleware).get(getChangePassword).post(postChangePassword);
 
-userRouter.get('/delete', handledelete);
-userRouter.get('/:id(\\d+)', see);
+userRouter.get('/:id', see);
 
 export default userRouter;
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import { watch, deleteVideo, getEdit, postEdit, upload, getUpload, postUpload } from '../controllers/videoController';
-import { protectorMiddleware } from '../middlewares';
+import { protectorMiddleware, videoUpload } from '../middlewares';
 
 const videoRouter = express.Router();
 
 videoRouter.get('/:id([0-9a-f]{24})', watch);
-videoRouter.route('/upload', upload).all(protectorMiddleware).get(getUpload).post(postUpload);
+videoRouter
+  .route('/upload', upload)
+  .all(protectorMiddleware)
+  .get(getUpload)
+  .post(videoUpload.single('video'), postUpload);
 videoRouter.route('/:id([0-9a-f]{24})/edit').all(protectorMiddleware).get(getEdit).post(postEdit);
 videoRouter.get('/:id([0-9a-f]{24})/delete', protectorMiddleware, deleteVideo);
 
